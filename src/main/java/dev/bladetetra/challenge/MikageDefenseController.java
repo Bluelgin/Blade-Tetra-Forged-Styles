e5858d52c740785d92dbdf3ed4bc072f60bce6cb
Taki <121283126+Bluelgin@users.noreply.github.com>
2026-09-25 18:00:23 +0800

    Move Mikage defense state types into defense controller

--- src/main/java/dev/bladetetra/challenge/MikageDefenseController.java
+++ src/main/java/dev/bladetetra/challenge/MikageDefenseController.java
@@ -17,18 +17,18 @@ final class MikageDefenseController {
     int swordWheelCounterCooldown;
     int swordWheelBreakTicks;
     final List<UUID> swordWheelEntities = new ArrayList<>();
-    final Map<UUID, MikageEntity.SaPattern> saPatterns = new HashMap<>();
-    final Map<UUID, MikageEntity.JudgementPattern> judgementPatterns = new HashMap<>();
+    final Map<UUID, SaPattern> saPatterns = new HashMap<>();
+    final Map<UUID, JudgementPattern> judgementPatterns = new HashMap<>();
     final Map<UUID, Long> hurtCooldownUntil = new HashMap<>();
     final Map<UUID, Long> lockBreakUntil = new HashMap<>();
-    final Map<UUID, MikageEntity.PursuitPressure> pursuitPressure = new HashMap<>();
+    final Map<UUID, PursuitPressure> pursuitPressure = new HashMap<>();
     boolean stepIaidoCommitted;
-    final Map<UUID, Deque<MikageEntity.MovementSample>> movementSamples = new HashMap<>();
-    final Map<UUID, MikageEntity.ShadowCrossPattern> shadowCrossPatterns = new HashMap<>();
+    final Map<UUID, Deque<MovementSample>> movementSamples = new HashMap<>();
+    final Map<UUID, ShadowCrossPattern> shadowCrossPatterns = new HashMap<>();
     int zanshinCounterTicks;
     UUID zanshinCounterTarget;
     Vec3 zanshinCounterCenter = Vec3.ZERO;
-    final Map<UUID, MikageEntity.PlayerDefenseProfile> playerDefenseProfiles = new HashMap<>();
+    final Map<UUID, PlayerDefenseProfile> playerDefenseProfiles = new HashMap<>();
     int mirrorCounterCooldown;
     long lastParryVfxTick = Long.MIN_VALUE;
 }
